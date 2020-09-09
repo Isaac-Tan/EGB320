4e9eb5e893b91821bedea031ce5f5abf57fb2d50
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-09-09 18:11:31 +1000

    Create RecordVid.py
    A program to record video on the Pi Cam

---
+++ RecordVid.py
@@ -0,0 +1,12 @@
+import picamera
+from time import *
+from subprocess  import call 
+
+with picamera.PiCamera() as camera:
+    camera.start_recording("pivid.h264")
+    sleep(5)
+    camera.stop_recording()
+    
+command = "MP4Box -add pivid.h264 pivid.mp4"
+call([command], shell=True)
+print("vid conv")
