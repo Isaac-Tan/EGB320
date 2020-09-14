326ca04d4ddf42431da82795f65e0312c410935b
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-09-14 16:20:46 +1000

    Update Vision.py
    Created function that does all vision. Main executes this for the sample, rock, obstacle.
    Created classes for sample, rock, obs

--- Vision.py
+++ Vision.py
@@ -6,7 +6,7 @@ import argparse
 import imutils
 import time
 
-FREQUENCY = 40 #Hz
+FREQUENCY = 10 #Hz
 INTERVAL = 1.0/FREQUENCY
 FOCAL_LEN = 3.04 #mm
 SENSOR_HEIGHT = 2.76 #mm
@@ -25,9 +25,58 @@ r_max_ = [int]*3
 o_min_ = [int]*3
 o_max_ = [int]*3
 
+s_max_arr = []
+s_min_arr = []
+r_max_arr = []
+r_min_arr = []
+o_max_arr = []
+o_min_arr = []
+
+Sample_list = []
+Rock_list = []
+Obstacle_list = []
+
 init = False
 
-cap = None
+class Sample:
+	#'Class for Samples'
+	sampleCount = 0
+	def __init__(self, ID, Dist, Bearing, cX, cY):
+		self.Dist = Dist
+		self.Bearing = Bearing
+		self.ID = ID
+		self.cX = cX
+		self.cY = cY
+		Sample.sampleCount += 1
+
+	def __del__(self):
+		Sample.sampleCount -= 1
+
+class Rock:
+	#'Class for Rocks'
+	rockCount = 0
+	def __init__(self, ID, Dist, Bearing, cX, cY):
+		self.Dist = Dist
+		self.Bearing = Bearing
+		self.ID = ID
+		self.cX = cX
+		self.cY = cY
+		Rock.rockCount += 1
+	def __del__(self):
+		Rock.rockCount -= 1
+
+class Obstacle:
+	#'Class for Obstacles'
+	obstacleCount = 0
+	def __init__(self, ID, Dist, Bearing, cX, cY):
+		self.Dist = Dist
+		self.Bearing = Bearing
+		self.ID = ID
+		self.cX = cX
+		self.cY = cY
+		Obstacle.obstacleCount += 1
+	def __del__(self):
+		Obstacle.obstacleCount -= 1
 
 def bounds():
 	##Gets the HSV values from the .txt files
@@ -49,6 +98,76 @@ def bounds():
 		o_min_[i] = (int(f.readline()))
 		o_max_[i] = (int(f.readline()))
 	f.close()
+
+	global s_min_arr
+	s_min_arr = np.array(s_min_)
+	global s_max_arr
+	s_max_arr = np.array(s_max_)
+
+	global r_min_arr
+	r_min_arr = np.array(r_min_)
+	global r_max_arr
+	r_max_arr = np.array(r_max_)
+
+	global o_min_arr
+	o_min_arr = np.array(o_min_)
+	global o_max_arr
+	o_max_arr = np.array(o_max_)
+
+
+def thresh(input_frame, type, total_img):
+	gray = input_frame[:, :, 2]
+	thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+	kernel = np.ones((5,5),np.uint8)
+	dilation = cv2.dilate(thresh,kernel,iterations = 5)
+	erosion = cv2.erode(dilation,kernel,iterations = 10)
+	opened = cv2.dilate(erosion,kernel,iterations = 5)
+	blurred_thresh = cv2.GaussianBlur(opened, (5, 5), 0)
+	ims = blurred_thresh
+	cnts = cv2.findContours(ims, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	i = 0
+	# loop over the contours
+	for c in cnts:
+		# compute the center of the contour
+		M = cv2.moments(c)
+		cX = int(M["m10"] / M["m00"])#Centre x-coord
+		cY = int(M["m01"] / M["m00"])#Centre y-coord
+		# compute bearing of the contour
+		bearing = round(31.1 * ((cX - (WIDTH/2.0))/(WIDTH/2.0)),3)
+		# get height/width of contour
+		x,y,h,w = cv2.boundingRect(c)
+		#calculate distance
+		dist = round(0.1*(FOCAL_LEN*SAMPLE_HEIGHT*HEIGHT)/(h*SENSOR_HEIGHT),3)
+		cv2.drawContours(total_img, [c], -1, (0, 255, 0), 2)
+		cv2.circle(total_img, (cX, cY), 7, (255, 0, 0), -1)
+		cv2.putText(total_img, "R: " + str(dist) + "cm", (cX - 15, cY + 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+		cv2.putText(total_img, "B: " + str(bearing), (cX - 15, cY + 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+		if (type == 0):
+			global Sample_list
+			sample = Sample(i,dist,bearing,cX,cY)
+			Sample_list[i] = sample
+			cv2.putText(total_img, "Sample", (cX - 15, cY - 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+		elif (type == 1):
+			global Rock_list
+			rock = Rock(i,dist,bearing,cX,cY)
+			Rock_list.append(Rock(i,dist,bearing,cX,cY))
+			cv2.putText(total_img, "Rock", (cX - 15, cY - 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+		elif (type == 2):
+			global Obstacle_list
+			obstacle = Obstacle(i,dist,bearing,cX,cY)
+			Obstacle_list.append(Obstacle(i,dist,bearing,cX,cY))
+			del obstacle
+			cv2.putText(total_img, "Obstacle", (cX - 15, cY - 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+		# elif (type == 3):
+		# 	Lander[c] = Lander(c,dist,bearing,cX,cY)
+		i = i + 1
+		return total_img
 
 
 def capture():
@@ -85,15 +204,6 @@ def process(frame):
 	frame = cv2.rotate(frame, cv2.ROTATE_180)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	s_min_arr = np.array(s_min_)
-	s_max_arr = np.array(s_max_)
-
-	r_min_arr = np.array(r_min_)
-	r_max_arr = np.array(r_max_)
-
-	o_min_arr = np.array(o_min_)
-	o_max_arr = np.array(o_max_)
-
 	s_mask = cv2.inRange(hsv, s_min_arr, s_max_arr)
 	r_mask = cv2.inRange(hsv, r_min_arr, r_max_arr)
 	o_mask = cv2.inRange(hsv, o_min_arr, o_max_arr)
@@ -104,124 +214,21 @@ def process(frame):
 	rock_img = cv2.bitwise_and(blurred, blurred, mask= r_mask)
 	obstacle_img = cv2.bitwise_and(blurred, blurred, mask= o_mask)
 
-	total_img = sample_img + rock_img + obstacle_img
+	# total_img = sample_img + rock_img + obstacle_img
+	total_img = frame
 
 
 	# cv2.imshow("Sample",sample_img)
 	# cv2.imshow("Rock", rock_img)
 	# cv2.imshow("Obstacle", obstacle_img)
+	Sample_list.clear()
+	Rock_list.clear()
+	Obstacle_list.clear()
+	sample = thresh(sample_img, 0, total_img)
+	rock = thresh(rock_img, 1,total_img)
+	obstacle = thresh(obstacle_img, 2,total_img)
 
-	gray_s = sample_img[:, :, 2]
-	gray_r = rock_img[:, :, 2]
-	gray_o = obstacle_img[:, :, 2]
-	
-	thresh_s = cv2.threshold(gray_s, 0, 255, cv2.THRESH_BINARY)[1]
-	thresh_r = cv2.threshold(gray_r, 0, 255, cv2.THRESH_BINARY)[1]
-	thresh_o = cv2.threshold(gray_o, 0, 255, cv2.THRESH_BINARY)[1]
-	kernel = np.ones((5,5),np.uint8)
-	# opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-	# closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-	dilation_s = cv2.dilate(thresh_s,kernel,iterations = 5)
-	dilation_r = cv2.dilate(thresh_r,kernel,iterations = 5)
-	dilation_o = cv2.dilate(thresh_o,kernel,iterations = 5)
-
-	erosion_s = cv2.erode(dilation_s,kernel,iterations = 10)
-	erosion_r = cv2.erode(dilation_r,kernel,iterations = 10)
-	erosion_o = cv2.erode(dilation_o,kernel,iterations = 10)
-
-	opened_s = cv2.dilate(erosion_s,kernel,iterations = 5)
-	opened_r = cv2.dilate(erosion_r,kernel,iterations = 5)
-	opened_o = cv2.dilate(erosion_o,kernel,iterations = 5)
-
-	blurred_thresh_s = cv2.GaussianBlur(opened_s, (5, 5), 0)
-	blurred_thresh_r = cv2.GaussianBlur(opened_r, (5, 5), 0)
-	blurred_thresh_o = cv2.GaussianBlur(opened_o, (5, 5), 0)
-
-	ims_s = blurred_thresh_s
-	ims_r = blurred_thresh_r
-	ims_o = blurred_thresh_o
-
-	# find contours in the thresholded image
-	cnts_s = cv2.findContours(ims_s, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cnts_s = imutils.grab_contours(cnts_s)
-
-	# loop over the contours
-	for c in cnts_s:
-		# compute the center of the contour
-		M = cv2.moments(c)
-		cX = int(M["m10"] / M["m00"])#Centre x-coord
-		cY = int(M["m01"] / M["m00"])#Centre y-coord
-		# compute bearing of the contour
-		bearing = round(31.1 * ((cX - (WIDTH/2.0))/(WIDTH/2.0)),3)
-		# draw the contour and center of the shape on the image
-		cv2.drawContours(total_img, [c], -1, (0, 255, 0), 2)
-		cv2.circle(total_img, (cX, cY), 7, (255, 0, 0), -1)
-		# get height/width of contour
-		x,y,h,w = cv2.boundingRect(c)
-		#calculate distance
-		dist = round(0.1*(FOCAL_LEN*SAMPLE_HEIGHT*HEIGHT)/(h*SENSOR_HEIGHT),3)
-		cv2.putText(total_img, "R: " + str(dist) + "cm", (cX - 15, cY + 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "B: " + str(bearing), (cX - 15, cY + 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "Sample", (cX - 15, cY - 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		# show the image
-		# cv2.imshow("Image", total_img)
-		#cv2.waitKey(0)
-		# find contours in the thresholded image
-	cnts_r = cv2.findContours(ims_r, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cnts_r = imutils.grab_contours(cnts_r)
-
-	# loop over the contours
-	for c in cnts_r:
-		# compute the center of the contour
-		M = cv2.moments(c)
-		cX = int(M["m10"] / M["m00"])#Centre x-coord
-		cY = int(M["m01"] / M["m00"])#Centre y-coord
-		# compute bearing of the contour
-		bearing = round(31.1 * ((cX - (WIDTH/2.0))/(WIDTH/2.0)),3)
-		# draw the contour and center of the shape on the image
-		cv2.drawContours(total_img, [c], -1, (0, 255, 0), 2)
-		cv2.circle(total_img, (cX, cY), 7, (255, 0, 0), -1)
-		# get height/width of contour
-		x,y,h,w = cv2.boundingRect(c)
-		#calculate distance
-		dist = round(0.1*(FOCAL_LEN*ROCK_HEIGHT*HEIGHT)/(h*SENSOR_HEIGHT),3)
-		cv2.putText(total_img, "R: " + str(dist) +"cm", (cX - 15, cY + 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "B: " + str(bearing), (cX - 15, cY + 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "Rock", (cX - 15, cY - 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-	
-		# find contours in the thresholded image
-	cnts_o = cv2.findContours(ims_o, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cnts_o = imutils.grab_contours(cnts_o)
-
-	# loop over the contours
-	for c in cnts_o:
-		# compute the center of the contour
-		M = cv2.moments(c)
-		cX = int(M["m10"] / M["m00"])#Centre x-coord
-		cY = int(M["m01"] / M["m00"])#Centre y-coord
-		# compute bearing of the contour
-		bearing = round(31.1 * ((cX - (WIDTH/2.0))/(WIDTH/2.0)),3)
-		# draw the contour and center of the shape on the image
-		cv2.drawContours(total_img, [c], -1, (0, 255, 0), 2)
-		cv2.circle(total_img, (cX, cY), 7, (255, 0, 0), -1)
-		# get height/width of contour
-		x,y,h,w = cv2.boundingRect(c)
-		#calculate distance
-		dist = round(0.1*(FOCAL_LEN*OBST_HEIGHT*HEIGHT)/(h*SENSOR_HEIGHT),3)
-		cv2.putText(total_img, "R: " + str(dist) + "cm", (cX - 15, cY + 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "B: " + str(bearing), (cX - 15, cY + 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-		cv2.putText(total_img, "Obstacle", (cX - 15, cY - 20),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-	#cv2.namedWindow('Thresholder_App', cv2.WINDOW_NORMAL) #sets window as resizable
-			# draw a line down the centre of the screen
+	# draw a line down the centre of the screen
 	cv2.line(total_img, ((int(WIDTH/2)),0), ((int(WIDTH/2)),int(HEIGHT)), (255, 255, 255))
 	
 
@@ -240,8 +247,11 @@ def cleanUp():
 	# Closes all the frames
 	cv2.destroyAllWindows()
 
+def main():
+	capture()
+
 if __name__ == '__main__':
 	if (init == False):
 		bounds()
 		init = True
-	capture()
+	main()
