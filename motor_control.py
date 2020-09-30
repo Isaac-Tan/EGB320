485390520481ba61822bea4af3c8a99406afc39e
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-09-30 17:59:30 +1000

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -21,18 +21,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(24 , GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 # Left, Right
-dir1 = [gpiozero.OutputDevice(23), gpiozero.OutputDevice(26)]
-dir2 = [gpiozero.OutputDevice(18), gpiozero.OutputDevice(19)]
+# dir1 = [gpiozero.OutputDevice(23), gpiozero.OutputDevice(26)]
+# dir2 = [gpiozero.OutputDevice(18), gpiozero.OutputDevice(19)]
+dir1 = [gpiozero.OutputDevice(18), gpiozero.OutputDevice(26)]
+dir2 = [gpiozero.OutputDevice(23), gpiozero.OutputDevice(19)]
 pwm = [GPIO.PWM(24, 100), GPIO.PWM(13, 100)]
-
-# dir1[0] = gpiozero.OutputDevice(23)
-# dir1[1] = gpiozero.OutputDevice(26)
-
-# dir2[0] = gpiozero.OutputDevice(18)
-# dir2[1] = gpiozero.OutputDevice(19)
-
-# pwm[0] = GPIO.PWM(24, 100)
-# pwm[1] = GPIO.PWM(13, 100)
+pwm[0].start(0)
+pwm[1].start(0)
 
 def motor(mot, value):
   if (value > 0):
@@ -43,14 +38,14 @@ def motor(mot, value):
     dir2[mot].on()
   pwm[mot].ChangeDutyCycle(value)
 
-def drive(magnitude, rotation, t):
+def drive(magnitude, rotation, tsec):
   motor(0, magnitude - rotation)
-  motor(1, magnitude - rotation)
-  time.sleep(t)
+  motor(1, magnitude + rotation)
+  time.sleep(tsec)
   pwm[0].stop()
   pwm[1].stop()
 
-drive(1,0,10)
+drive(20,0,10)
 
 # def motor_control(speedR, speedL,  dirR, dirL, t):
 #   SpeedPWML.start(0)
