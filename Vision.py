5bc6ca65f1e6565225e57e359197280583aafb7e
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-07 20:00:58 +1000

    Update Vision.py

--- Vision.py
+++ Vision.py
@@ -23,7 +23,7 @@ pwm[0].start(0)
 pwm[1].start(0)
 #Multipliers for uneven motor power
 m1mult = 1.0 #Left motor multiplier
-m2mult = 0.9 #Right motor multiplier
+m2mult = 1.0 #Right motor multiplier
 
 
 FREQUENCY = 20 #Hz
