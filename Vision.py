7a36f05864796f4fabc9ba470b0fc234ea171b37
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-07 19:31:57 +1000

    Update Vision.py

--- Vision.py
+++ Vision.py
@@ -194,7 +194,6 @@ def bounds():
 	b_max_arr = np.array(b_max_)
 
 def motor(mot, value):
-	np.clip(value, -100, 100)
   	#Drive motor(which motor, PWM intensity)
 	if (value > 0):
     #if value is positive: drive forward
@@ -205,6 +204,8 @@ def motor(mot, value):
 		dir1[mot].off()
 		dir2[mot].on()
 	#set the pwm pin at index mot to "value"
+	if (abs(value) > 100):
+		value = 100
 	pwm[mot].ChangeDutyCycle(abs(value))
 
 def drive(magnitude, rotation):
