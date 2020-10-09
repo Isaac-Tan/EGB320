7d67118413f19b02af7a6c31cea4402520083cfa
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-09 13:08:33 +1000

    Update photocell.py

--- photocell.py
+++ photocell.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python  
 import time  
-import RPi.GPIO as GPIO  
+import RPi.GPIO as GPIO
+
+PHOTOCELL = 17
+
+GPIO.setmode(GPIO.BCM)  
   
-GPIO.setmode(GPIO.BOARD)  
-  
-def RCtime (RCpin):  
-        reading = 0  
-        GPIO.setup(RCpin, GPIO.OUT)  
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)  
-  
-        GPIO.setup(RCpin, GPIO.IN)  
-        while (GPIO.input(RCpin) == GPIO.LOW):  
-                reading += 1  
-        return reading  
+def RCtime():  
+    reading = 0  
+    GPIO.setup(PHOTOCELL, GPIO.OUT)  
+    GPIO.output(PHOTOCELL, GPIO.LOW)
+    time.sleep(0.1)  
+    GPIO.setup(PHOTOCELL, GPIO.IN)  
+    while (GPIO.input(PHOTOCELL) == GPIO.LOW):  
+        reading += 1  
+    return reading  
   
 while True:                                       
-        print RCtime(11)  
+    print RCtime()  
