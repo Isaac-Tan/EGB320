1618c1384258299c9fc06f8b7ee668f625f10cef
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-15 17:36:52 +1000

    Update photocell.py

--- photocell.py
+++ photocell.py
@@ -5,6 +5,8 @@ import RPi.GPIO as GPIO
 PHOTOCELL = 17
 
 GPIO.setmode(GPIO.BCM)  
+avg = []
+thresh = 0
   
 def RCtime():  
     reading = 0  
@@ -14,7 +16,13 @@ def RCtime():
     GPIO.setup(PHOTOCELL, GPIO.IN)  
     while (GPIO.input(PHOTOCELL) == GPIO.LOW):  
         reading += 1
-    return reading  
-  
-while True:                                       
-    print RCtime()  
+    return reading 
+
+if (len(avg) < 20):
+    avg.append(RCtime())
+else:
+    thresh = float(sum(avg) / len(avg))
+
+while True:
+    print("Thresh: ", thresh)
+    print(RCtime())
