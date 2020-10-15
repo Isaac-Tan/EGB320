0cbbb0f37277dc0f23f825bcc351da888459b3a8
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-15 15:52:02 +1000

    Update photocell.py
    Changed to digital read

--- photocell.py
+++ photocell.py
@@ -7,13 +7,7 @@ PHOTOCELL = 17
 GPIO.setmode(GPIO.BCM)  
   
 def RCtime():  
-    reading = 0  
-    GPIO.setup(PHOTOCELL, GPIO.OUT)  
-    GPIO.output(PHOTOCELL, GPIO.LOW)
-    time.sleep(0.1)  
-    GPIO.setup(PHOTOCELL, GPIO.IN)  
-    while (GPIO.input(PHOTOCELL) == GPIO.LOW):  
-        reading += 1  
+    reading = GPIO.input(PHOTOCELL)
     return reading  
   
 while True:                                       
