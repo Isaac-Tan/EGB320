f9d96e7df1fb6315fe167132234854773cc08f44
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-18 16:05:29 +1000

    Update integration.py

--- integration.py
+++ integration.py
@@ -536,7 +536,6 @@ def naviagtion():
 	rot = 0
 	#if it cant see the target
 	if (bdist == 0):
-		print("cant see")
 		#if last seen on the left
 		if (max_index < 160):
 			#turn left
@@ -573,17 +572,16 @@ def naviagtion():
 		else:
 			max_val = VEL_SCALE * bdist + VEL_MIN
 			rot = round(ROT_SCALE*bearing,2)
-	# if (captured == 1):
-	# 	LED(1)
-	# 	print("Go lander")
-	# else:
-	# 	if(bdist != 0):
-	# 		LED(2)
-	# 		print("sees targ")
-	# 	else:
-	# 		LED(3)
-	# 		print("looking for targ")
-	LED(2)
+	if (captured == 1):
+		LED(1)
+		print("Go lander")
+	else:
+		if(bdist != 0):
+			LED(2)
+			print("sees targ")
+		else:
+			LED(3)
+			print("looking for targ")
 	drive(max_val, -1*rot)
 
 	#drive(15,0)
