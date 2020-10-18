294842be5d9a2a08e9b8fd1bc49ebdf3f9307e32
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-19 04:26:39 +1000

    Update LEDtest.py

--- LEDtest.py
+++ LEDtest.py
@@ -8,9 +8,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(GREEN, GPIO.OUT) #Green LED
 GPIO.setup(YELLOW, GPIO.OUT) #Yellow LED
 GPIO.setup(RED, GPIO.OUT) #Red LED
-GPIO.output(GREEN, GPIO.LOW)
-GPIO.output(YELLOW, GPIO.LOW)
-GPIO.output(RED, GPIO.LOW)
 
 while (1):
     GPIO.output(GREEN, GPIO.HIGH)
