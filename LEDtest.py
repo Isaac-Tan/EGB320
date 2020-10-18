f4611736430f01fe3beac35d699c3828688b1f17
Isaac-Tan <Isaac-Tan@users.noreply.github.com>
2020-10-19 04:34:18 +1000

    Update LEDtest.py

--- LEDtest.py
+++ LEDtest.py
@@ -1,15 +1,24 @@
 import RPi.GPIO as GPIO
 
-RED = 16 #Red LED
+RED = 21 #Red LED
 YELLOW = 20 #Yellow LED
-GREEN = 21 #Green LED
+GREEN = 16 #Green LED
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GREEN, GPIO.OUT) #Green LED
 GPIO.setup(YELLOW, GPIO.OUT) #Yellow LED
 GPIO.setup(RED, GPIO.OUT) #Red LED
 
-while (1):
-    GPIO.output(GREEN, GPIO.HIGH)
-    # GPIO.output(YELLOW, GPIO.HIGH)
-    # GPIO.output(RED, GPIO.HIGH)
+counter = 0
+
+try: 
+    while (counter < 1000000):
+        GPIO.output(GREEN, GPIO.HIGH)
+        # GPIO.output(YELLOW, GPIO.HIGH)
+        # GPIO.output(RED, GPIO.HIGH)
+        counter += 1
+except:
+    print("/n", counter)
+finally:
+    GPIO.cleanup() # this ensures a clean exit  
+    
